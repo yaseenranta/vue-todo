bc1d7c5a40b76565d47ddd958d7f73e8efce611c
Muhammad Yaseen <yaseenranta76@gmail.com>
2020-12-30 17:38:06 +0500

    Auth initial work

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import todo from './modules/todo'
+import auth from './modules/auth'
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules : {
-    todo
+    todo,
+    auth
   },
 })
---
+++ src/store/modules/auth.js
@@ -0,0 +1,25 @@
+const state = () => ({
+
+})
+
+const mutations = {
+
+}
+
+
+const getters = {
+
+}
+
+
+const actions = {
+
+}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+  }
